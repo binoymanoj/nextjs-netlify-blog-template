b3dbc6a334f3eba001468cfe0373a8f58723fc3b
binoymanoj <binoypm2002@gmail.com>
2022-07-11 23:17:04 +0530

    footer is changed

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,19 @@
+import InstagramIcon from '@mui/icons-material/Instagram';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
 
 export default function Footer() {
     return (
         <footer>
-            <h3>About :</h3>
+            <h3>About :</h3> 
             <div className="container">
-                <a link href="https://www.instagram.com/binoy_manoj/">instagram</a> <br/>
-                <a link href="https://github.com/binoymanoj/">github</a> <br/>
-                <a link href="https://twitter.com/binoy_manoj_">twitter</a> <br/>
-                <a link href="https://www.linkedin.com/in/binoy-manoj-29b5a3223/">linkedIn</a> <br/>
-                Email: <a link href="mailto:binoypm2002@gmail.com">binoypm2002@gmail</a>
+                <a link href="https://www.instagram.com/binoy_manoj/" text-align> <InstagramIcon style={{ color: "white" }} fontSize="large" /> </a> <br/>
+                <a link href="https://github.com/binoymanoj/"> <GitHubIcon style={{ color: "white" }} fontSize="large" /> </a> <br/>
+                <a link href="https://twitter.com/binoy_manoj_"> < TwitterIcon style={{ color: "white" }} fontSize="large" /> </a> <br/>
+                <a link href="https://www.linkedin.com/in/binoy-manoj-29b5a3223/"> <LinkedInIcon style={{ color: "white" }} fontSize="large" /> </a> <br/>
+                <a link href="mailto:binoypm2002@gmail.com">< EmailIcon style={{ color: "white" }} fontSize="large" /></a>
             </div>
         </footer>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
   return (
   <>
