aa1f7ce2f9982e9c64b3106ba4e8a0503b41eef6
binoymanoj <binoypm2002@gmail.com>
2022-07-12 10:22:33 +0530

    footer and css

--- components/Footer.js
+++ components/Footer.js
@@ -8,13 +8,25 @@ export default function Footer() {
     return (
         <footer>
             <h3>About :</h3> 
-            <div className="container">
-                <a link href="https://www.instagram.com/binoy_manoj/" text-align> <InstagramIcon style={{ color: "white" }} fontSize="large" /> </a> <br/>
-                <a link href="https://github.com/binoymanoj/"> <GitHubIcon style={{ color: "white" }} fontSize="large" /> </a> <br/>
-                <a link href="https://twitter.com/binoy_manoj_"> < TwitterIcon style={{ color: "white" }} fontSize="large" /> </a> <br/>
-                <a link href="https://www.linkedin.com/in/binoy-manoj-29b5a3223/"> <LinkedInIcon style={{ color: "white" }} fontSize="large" /> </a> <br/>
+            <h4>
+                This blog is optimized for learning. Most of the contents are related to real-world hacking. 
+                You may use it for educational or informational purposes only. Hacking into other people is illegal.
+            </h4>
+
+            <div className="footer">
+                <h3>Contact me :</h3>
+                <a link href="https://www.instagram.com/binoy_manoj/" text-align> <InstagramIcon style={{ color: "white" }} fontSize="large" /> </a> 
+                <a link href="https://github.com/binoymanoj/"> <GitHubIcon style={{ color: "white" }} fontSize="large" /> </a> 
+                <a link href="https://twitter.com/binoy_manoj_"> < TwitterIcon style={{ color: "white" }} fontSize="large" /> </a> 
+                <a link href="https://www.linkedin.com/in/binoy-manoj-29b5a3223/"> <LinkedInIcon style={{ color: "white" }} fontSize="large" /> </a> 
                 <a link href="mailto:binoypm2002@gmail.com">< EmailIcon style={{ color: "white" }} fontSize="large" /></a>
             </div>
+
+            <h6>
+                Copyright &copy; binoy_manoj 2022 <br /> 
+                All rights reserved.
+            </h6>
+
         </footer>
     )
 }
